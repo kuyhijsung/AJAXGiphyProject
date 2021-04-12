2cfcfbdf702013b1e4b5910d8099217f9584472f
Andy <jihyuk.sung@gmail.com>
2021-04-12 14:29:32 -0600

    Finished

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const searchButton = document.querySelector('#searchButton');
 const removeButton = document.querySelector('#removeButton');
+const gifSection = document.querySelector('#gifSection');
+
 searchButton.addEventListener('click', async function (e) {
     e.preventDefault();
     const input = document.querySelector('#gifSearch');
     const searchTerm = input.value;
     const gifURL = await searchGifs(searchTerm);
     input.value = '';
-    const gifSection = document.querySelector('#gifSection');
     const img = document.createElement('img');
     img.src = gifURL;
     gifSection.append(img);
 })
 
-
+removeButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    while (gifSection.firstChild) {
+        gifSection.removeChild(gifSection.firstChild);
+    }
+})
 
 async function searchGifs(search) {
     const res = await axios.get('http://api.giphy.com/v1/gifs/search', {
@@ -21,6 +27,7 @@ async function searchGifs(search) {
             api_key: 'LuzmrXfvWZy3PuqBQIXP2965MdrfLmul'
         }
     });
-    const randomValue = Math.floor(Math.random() * 50);
+    const numResults = res.data.data.length;
+    const randomValue = Math.floor(Math.random() * numResults);
     return res.data.data[randomValue].images.original.url;
 }
